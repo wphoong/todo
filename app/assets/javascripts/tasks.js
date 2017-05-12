7f660d825a64514e6c21c1178bd2bff617d8c18c
Wilson <wphoong@gmail.com>
2017-05-12 09:06:11 +0000

    added line through to task when completed, removing when unchecked

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -5,7 +5,8 @@ $(function() {
     // <li> tags
     function taskHtml(task){
       var checkedStatus = task.done ? "checked" : "";
-      var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' + " data-id='" + task.id + "'" + checkedStatus + '><label>' + task.title + '</label></div></li>';
+      var liClass = task.done ? "completed" : "" ;
+      var liElement = '<li id=listItem-'+ task.id + ' class="' + liClass + '"><div class="view"><input class="toggle" type="checkbox"' + " data-id='" + task.id + "'" + checkedStatus + '><label>' + task.title + '</label></div></li>';
 
       return liElement;
     }
@@ -25,6 +26,11 @@ $(function() {
         task: {
           done: doneValue
         }
+      }).success(function(data){
+        var liHtml = taskHtml(data);
+        var $li = $("#listItem-" + data.id);
+        $li.replaceWith(liHtml);
+        $('.toggle').change(toggleTask);
       });
 
     }
