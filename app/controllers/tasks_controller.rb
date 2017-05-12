a6ae4a7a8705f9765ce25858110af05095300438
Wilson <wphoong@gmail.com>
2017-05-12 05:01:35 +0000

    added task create method in rspec and task controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -10,9 +10,15 @@ class TasksController < ApplicationController
 
   end
 
+  def create
+    task = Task.create(task_params)
+    render json: task
+
+  end
+
   private
 
   def task_params
-    params.require(:task).permit(:done)
+    params.require(:task).permit(:done, :title)
   end
 end
